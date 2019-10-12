cb550ee05972f03b45ca02be74a8d9b20e8dabc0
Aji Suryawan <kazumovie132@gmail.com>
2019-10-12 11:46:30 +0700

    update

--- app/src/main/java/com/example/rusmart/UI/LogOut.java
+++ app/src/main/java/com/example/rusmart/UI/LogOut.java
@@ -1,13 +1,16 @@
 package com.example.rusmart.UI;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.rusmart.Model.PrefManager;
 import com.example.rusmart.R;
@@ -31,21 +34,32 @@ public class LogOut extends AppCompatActivity {
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final DialogInterface.OnClickListener dialog = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                SharedPreferences mLogin = getSharedPreferences("login", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = mLogin.edit();
-                editor.clear();
-                editor.apply();
+                        switch (i) {
 
+                            case DialogInterface.BUTTON_POSITIVE:
+                                SharedPreferences mLogin = getSharedPreferences("login", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = mLogin.edit();
+                                editor.clear();
+                                editor.apply();
+                                Intent intent = new Intent(LogOut.this, SplashScreen.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
 
-                Intent intent = new Intent(LogOut.this, SplashScreen.class);
-                startActivity(intent);
-                finish();
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                Toast.makeText(getApplicationContext(), "Data gagal di simpan", Toast.LENGTH_LONG).show();
+                                break;
+                        }
 
+                    }
+                };
 
-                PrefManager prefManager = new PrefManager(getApplicationContext());
-                prefManager.setFirstTimeLaunch(true);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Apakah anda yakin ingin keluar?").setPositiveButton("Ya", dialog)
+                        .setNegativeButton("Tidak", dialog).show();
 
             }
         });
