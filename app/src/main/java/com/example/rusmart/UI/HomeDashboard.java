1fc59bdaa472654ca25ccddd9b831a808d47c488
DeaAnandita <dea2018rplrus2@gmail.com>
2019-10-14 14:52:27 +0700

    profile layout

--- app/src/main/java/com/example/rusmart/UI/HomeDashboard.java
+++ app/src/main/java/com/example/rusmart/UI/HomeDashboard.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.rusmart.R;
 
@@ -19,6 +23,8 @@ public class HomeDashboard extends AppCompatActivity {
 
     CardView cdCatatTagihan;
     Realm realm;
+    Button btnLogOut;
+    TextView namelogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,53 @@ public class HomeDashboard extends AppCompatActivity {
         realm = Realm.getInstance(configuration);
 
         cdCatatTagihan = findViewById(R.id.cdCatatTagihan);
+        btnLogOut = findViewById(R.id.btnLogOut);
+        namelogout = findViewById(R.id.namelogout);
+        SharedPreferences mLogin = getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        namelogout.setText(mLogin.getString("username", "missing"));
+
+        if (getSharedPreferences("login", Context.MODE_PRIVATE) != null) {
+
+            namelogout.setText(mLogin.getString("username", "missing"));
+        }
+
 
         cdCatatTagihan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(HomeDashboard.this, CatatPembelian.class);
                 startActivity(intent);
+            }
+        });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogInterface.OnClickListener dialog = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        switch (i) {
+
+                            case DialogInterface.BUTTON_POSITIVE:
+                                SharedPreferences mLogin = getSharedPreferences("login", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = mLogin.edit();
+                                editor.clear();
+                                editor.apply();
+                                Intent intent = new Intent(HomeDashboard.this, SplashScreen.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+
+                        }
+
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setMessage("Apakah anda yakin ingin keluar?").setPositiveButton("Ya", dialog)
+                        .setNegativeButton("Tidak", dialog).show();
+
             }
         });
 
