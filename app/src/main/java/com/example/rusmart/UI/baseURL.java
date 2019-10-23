55c6c8efaf498077bcaf5b7430f894e6c900a0af
Aji Suryawan <kazumovie132@gmail.com>
2019-10-23 07:41:10 +0700

    makan

--- app/src/main/java/com/example/rusmart/UI/baseURL.java
+++ app/src/main/java/com/example/rusmart/UI/baseURL.java
@@ -2,8 +2,8 @@ package com.example.rusmart.UI;
 
 public class baseURL {
     //http://192.168.6.126/
-    //public static final String baseurl="http://192.168.43.123/";
-    //public static final String baseurl="http://192.168.6.126/";
-    public static final String baseurl="http://192.168.6.23/";
+    //public static final String baseurl="http://192.168.43.123/api/";
+    //public static final String baseurl="http://192.168.6.126/api/";
+    public static final String baseurl="http://192.168.6.23/api/";
 
 }
